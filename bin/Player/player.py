b48f1e415a22ec03d4b1e483530d3d49d77950c6
watislaf <vladkozulin@mail.ru>
2021-11-06 12:52:34 +0300

    Players concept created

--- bin/Player/player.py
+++ bin/Player/player.py
@@ -1,2 +1,3 @@
 
 class Player:
+    pass
--- bin/controller.py
+++ bin/controller.py
@@ -1,13 +1,21 @@
+from bin.Player.player import Player
 from bin.view.view import View
 
 
 class Controller:
     view = View()
-    white_player = None
-    black_player = None
+    white_player_ = None
+    black_player_ = None
+
     def start_view(self):
         self.view.start()
         pass
 
     def start_game(self):
         pass
+
+    def set_white_payer(self, player: Player):
+        self.white_player_ = player
+
+    def set_black_payer(self, player: Player):
+        self.black_player_ = player
