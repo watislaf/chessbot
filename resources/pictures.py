82ee453eb6be12deacf00b4716260dd8bbb735cc
Vlad Kozulin <45079123+watislaf@users.noreply.github.com>
2024-02-10 10:58:04 +0100

    Update pictures.py

--- resources/pictures.py
+++ resources/pictures.py
@@ -8,7 +8,6 @@ if hasattr(sys, '_MEIPASS'):
     RESOURCES_PATH=sys._MEIPASS
 else:
     RESOURCES_PATH = '/'.join(__file__.split("/")[:-1])
-
 GRAY_COLOR = [122, 122, 122]
 WHITE_BOARD_COLOR = [210, 238, 238]
 BLACK_BOARD_COLOR = [86, 150, 118]
